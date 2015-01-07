6372b1918180669854892988042ae5eeab8a1aee
huayun321 <huayun321@gmail.com>
2015-01-07 11:12:31 +0800

    initial semantic ui

---
+++ public/javascripts/index_with_modal.js
@@ -0,0 +1,20 @@
+function show_detail() {
+    $('#mindex')
+        .modal('show')
+    ;
+};
+function show_img() {
+    $('#imodal')
+        .modal('show')
+    ;
+};
+
+function show_user() {
+    $('#user_modal')
+        .modal('show')
+    ;
+};
+
+$( document ).ready(function() {
+
+});
